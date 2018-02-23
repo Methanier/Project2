b7dd0980ed63bf64130e4d3a61a27da81fd1bbbf
Charles Cozart <charlescozart24@gmail.com>
2018-02-23 16:09:46 -0700

    init

---
+++ Classes/Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance { get; private set; }
+
+    private string filePath;
+
+    [Header("Player Info")]
+    public PlayInfo playerInfo;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        filePath = Application.persistentDataPath + "/" + Application.productName + "gameInfo.txt";
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("save");
+            SaveInfo();
+        }
+        if(Input.GetKey(KeyCode.L))
+        {
+            Debug.Log("load");
+            LoadInfo();
+        }
+    }
+
+    public void SaveInfo()
+    {
+        string infoJSON = JsonUtility.ToJson(playerInfo);
+
+        File.WriteAllText(filePath, infoJSON);
+    }
+
+    public void LoadInfo()
+    {
+        string infoJSON = File.ReadAllText(filePath);
+
+        playerInfo = JsonUtility.FromJson<PlayInfo>(infoJSON);
+    }
+}
---
+++ Classes/Assets/Scripts/PlayInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[Serializable]
+public class PlayInfo {
+
+    public string playerName;
+    public int playerExperience;
+    public float playerHealth;
+}
